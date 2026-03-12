90d65403103fc229483f0ee2dda3cfbd1aaa2799
Goobley <Christopher.Osborne@glasgow.ac.uk>
2026-03-12 17:40:41 +0000

    Add missing header

---
+++ include/profiling.h
@@ -0,0 +1,15 @@
+#ifndef SAMS_PROFILING_H
+#define SAMS_PROFILING_H
+
+#ifdef USE_ROCTX
+#include <roctx.h>
+#define PROF_PUSH(NAME) roctxRangePushA(NAME)
+#define PROF_POP() roctxRangePop()
+#define PROF_MARK(NAME) roctxMarkA(NAME)
+#else
+#define PROF_PUSH(NAME) ((void)0)
+#define PROF_POP() ((void)0)
+#define PROF_MARK(NAME) ((void)0)
+#endif
+
+#endif // SAMS_PROFILING_H
